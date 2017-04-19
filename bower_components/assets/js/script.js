86f0c428f4ad00f30bef37765af63c7f74644f3e
handson-ekkalak <ekkalak@hands-onconsultants.com>
2017-04-19 14:01:34 +0700

    Update bower components

---
+++ bower_components/assets/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Created by ekkES on 4/19/2017.
+ */
+;var MyApp = MyApp || {};
+// ;(function($) {
+//
+// })(jQuery);
+
+
+jQuery(document).ready(function($){
+    //alert('test');
+});
+
+jQuery(window).resize(function($){
+});
+
+jQuery(window).on(function($){
+});
